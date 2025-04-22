366e7b7fb5370c6ffbc114291612496139f5c6fe
amv0048 <amv0048@alu.medac.es>
2025-04-22 18:36:38 +0200

    Update Minion.java

--- Herencia/Ejercicio96/Personaje/Minion.java
+++ Herencia/Ejercicio96/Personaje/Minion.java
@@ -6,16 +6,11 @@ public class Minion extends Personaje{
     public Minion(String tipo, String nombre, 
             int x, int y, int escudo, int ataque, int vida) {
         super(nombre, vida, x, y, escudo, ataque);
-        if (!tipo.equalsIgnoreCase("Melee") ||
-               !tipo.equalsIgnoreCase("Mago") ||
-                !tipo.equalsIgnoreCase("Cannon"))
-            this.tipo = "Melee";
-        else this.tipo = tipo;
         this.nombre = "minion";
         this.x = (int)(Math.random() * (400-1) + 1);
         this.y = (int)(Math.random() * (1520-1) + 1);
         
-        if (tipo.equals("Melee")){
+        if (tipo.equals("Cannon")){
             this.vida = 50;
             this.escudo = 10;
             this.ataque = 10;
@@ -26,6 +21,7 @@ public class Minion extends Personaje{
             this.ataque = 30;
         }
         else{
+            this.tipo = "Melee";
             this.vida = 75;
             this.escudo = 5;
             this.ataque = 25;
