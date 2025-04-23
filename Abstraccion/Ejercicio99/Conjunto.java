33daf00095fddff01fad0738bed6a6e9297b884a
amv0048 <amv0048@alu.medac.es>
2025-04-23 19:18:49 +0200

    Update Conjunto.java

--- Abstraccion/Ejercicio99/Conjunto.java
+++ Abstraccion/Ejercicio99/Conjunto.java
@@ -1,9 +1,8 @@
 package Abstraccion.Ejercicio99;
 
 public class Conjunto extends Coleccion {
-    private static final int TAM = 10;
-    
-    public Conjunto(){
+
+    public Conjunto() {
         super();
     }
     
@@ -11,26 +10,28 @@ public class Conjunto extends Coleccion {
         super(num);
     }
     
-    // Metodos abstractos
     @Override
     public void aniadir(int elemento) {
         if (!contiene(elemento) && !lleno()){
             setElemento(elemento);
+            this.contador++;
         }
-        else {
-            
-            
-        }
+        /*else {
+            int nuevoArray[] = new int[this.array.length*2];
+            setArray(nuevoArray);
+            this.contador++;
+        }*/
     }
 
     @Override
     public void eliminar(int elemento) {
         boolean eliminado = false;
         int i = 0;
-        while (i <= this.getElementos() && !eliminado){
-            if (getArray()[i] == elemento){
-                getArray()[i] = 0;
+        while (i < this.array.length && !eliminado){
+            if (this.array[i] == elemento){
+                this.array[i] = -1;
                 eliminado = true;
+                this.contador--;
             }
             i++;
         }
@@ -39,33 +40,31 @@ public class Conjunto extends Coleccion {
     public Conjunto union(Conjunto a){
         int cont = 0;
         
-        for (int i = 0; i < getElementos(); i++) {
-            if (!contiene(a.array[i]))
+        for (int i = 0; i <= a.getElementos(); i++) {
+            if (!this.contiene(a.array[i]))
                 cont++;
         }
-        cont += this.contador;
-        
+        cont += this.getElementos();
         Conjunto result = new Conjunto(cont);
         
-        for (int i = 0; i < this.contador; i++) {
-            result.aniadir(getArray()[i]);
+        for (int i = 0; i < result.array.length; i++) {
+            result.aniadir(this.array[i]);
         }
-        for (int i = 0; i < this.contador; i++) {
-            if (!result.contiene(a.array[i]))
-                result.aniadir(a.array[i]);
+        for (int i = 0; i < result.array.length; i++) {
+            result.aniadir(a.array[i]);
         }
         System.out.println("cont: "+cont);
-        System.out.println("a: "+this.contador);
-        System.out.println("this: "+this.contador);
+        System.out.println("a: "+a.getElementos());
+        System.out.println("this: "+this.getElementos());
         System.out.println("result: "+result.array.length);
-        
+
         return result;
     }
     
     public Conjunto interseccion(Conjunto a){
         int cont = 0;
-        for (int i = 0; i < this.getElementos(); i++) {
-            if (a.contiene(this.getArray()[i]))
+        for (int i = 0; i <= this.getElementos(); i++) {
+            if (a.contiene(this.array[i]))
                 cont++;
         }
         Conjunto result = new Conjunto(cont);
@@ -76,17 +75,4 @@ public class Conjunto extends Coleccion {
         }
         return result;
     }
-
-    @Override
-    public int getElementos() {
-        return super.getElementos(); 
-    }
-    
-    
-    
-    
-    
-    // Utils
-    
-    
 }
