d39d09bafdc0272d97de9a4758cc6ae6e2d1d06f
amv0048 <amv0048@alu.medac.es>
2025-04-24 18:43:08 +0200

    Update Notas.java

--- Abstraccion/Ejercicio102/Notas.java
+++ Abstraccion/Ejercicio102/Notas.java
@@ -1,6 +1,27 @@
 package Abstraccion.Ejercicio102;
 
+public class Nota {
+    private String valor;
+    private int duracion; // Segundos
 
-public class Notas {
-
+    public Nota(String valor, int duracion) {
+        if (duracion >= 1 && duracion <= 10)
+            this.duracion = duracion;
+        else this.duracion = 1;
+        
+        if (!valor.equals("do") || !valor.equals("re")|| !valor.equals("mi") || 
+                !valor.equals("fa") ||!valor.equals("sol")
+                ||!valor.equals("la") ||!valor.equals("si"))
+            this.valor = "do";
+    }
+    
+    public Nota() {
+        this.valor = "do";
+        this.duracion = 1;
+    }
+    
+    @Override
+    public String toString(){
+        return "["+this.valor+","+this.duracion+"]";
+    }
 }
