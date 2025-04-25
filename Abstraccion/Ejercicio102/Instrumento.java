771582bcaf78f165cf9150d0cabff3af72e02880
amv0048 <amv0048@alu.medac.es>
2025-04-25 13:16:34 +0200

    Update Instrumento.java

--- Abstraccion/Ejercicio102/Instrumento.java
+++ Abstraccion/Ejercicio102/Instrumento.java
@@ -1,13 +1,14 @@
 package Abstraccion.Ejercicio102;
 
-public class Instrumento {
+public abstract class Instrumento {
     private String marca;
     private String fecha;
     private Partitura musica;
     private boolean tocando;
     private boolean roto;
 
-    public Instrumento(String marca, String fecha, Partitura musica) {
+    public Instrumento(String marca, String fecha,
+            Partitura musica) {
         this.marca = marca;
         this.fecha = fecha;
         this.musica = musica;
@@ -18,16 +19,26 @@ public class Instrumento {
     public Instrumento() {
         this.marca = "desconocido";
         this.fecha = this.marca;
-        int tiempo = (int)(Math.random() * 4 + 1);
         int numNotas = (int)(Math.random() * 90 + 10);
-        int valorR = (int)(Math.random() * 6 + 1);
-        for (int i = 0; i < numNotas; i++) {
-            musica.aniadirNota(new Nota("do",tiempo));
+        
+        musica = new Partitura(20);
+        
+        for (int i = 0; i <= numNotas; i++) {
+            int valorR = (int)(Math.random() * 6 + 1);
+            int tiempo = (int)(Math.random() * 4 + 1);
+            musica.aniadirNota(new Nota(valorR,tiempo));
         }
     }
     
     public String tocarPartitura(){
-        
+        String res="";
+        if (!tocando && !roto){
+            for (int i = 0; i < musica.getLista().length; i++) {
+                res += musica.getLista()[i];
+                
+            }
+        }
+        return res;
     }
     
     public String pararDeTocar(){
@@ -36,6 +47,6 @@ public class Instrumento {
     }
     
     
-    
+    public abstract void afinarse();
     
 }
