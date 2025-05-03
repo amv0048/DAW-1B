1366874f51f54c6033dcede4cf684fccd284fe7d
amv0048 <amv0048@alu.medac.es>
2025-05-03 11:15:09 +0200

    Add files via upload

---
+++ Abstraccion/Ejercicio100/Main.java
@@ -0,0 +1,19 @@
+package Abstraccion.Ejercicio100;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+            Articulo[] articulos = {
+            new Articulo("Teclado", 80),
+            new Articulo("Raton", 40)};
+            
+            Cliente c = new Premium("Valeria", "abc12", 1234567891) ;
+            c.realizarCompra(new Date(), articulos);
+            System.out.println(c.mostrarHistorial());
+            
+    }
+    
+    
+}
