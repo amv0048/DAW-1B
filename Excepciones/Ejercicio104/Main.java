31bbb1991566b0bbf40722105c137b7efa47ae54
amv0048 <amv0048@alu.medac.es>
2025-05-07 21:52:36 +0200

    Update Main.java

--- Excepciones/Ejercicio104/Main.java
+++ Excepciones/Ejercicio104/Main.java
@@ -3,7 +3,12 @@ package Excepciones.Ejercicio104;
 public class Main {
 
     public static void main(String[] args) {
-        
+        System.out.println("Programa automatico");
+        try {
+            throw new RuntimeException("soy una excepcion");
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Programa terminado");
     }
-
 }
